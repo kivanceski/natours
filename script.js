fc80f84283a62ba99378b57d32ea0799e9c60494
Kivanc Eski <eskikivanc@gmail.com>
2021-10-20 16:08:05 +0300

    fixed modal js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const navList = document.querySelector(".navigation__list");
 const navCheckbox = document.querySelector(".navigation__checkbox");
 const popup = document.querySelector(".popup");
 const popupContent = document.querySelector(".popup__content");
-const popupTarget = document.querySelector(".popup:target");
 const popupButtons = document.querySelectorAll(".btn-popup");
 const popupClose = document.querySelector(".popup__close");
 
@@ -18,19 +17,23 @@ navList.addEventListener("click", function (e) {
 
 popupButtons.forEach((btn) => {
   btn.addEventListener("click", function (e) {
-    popup.classList.add("visible");
-    popupContent.classList.add("visible");
-    popupContent.classList.add("zoom-in");
+    popup.classList.remove("hidden");
+    popupContent.classList.remove("hidden");
+    popupContent.classList.add("scale");
   });
 });
 
 popup.addEventListener("click", function (e) {
   const element = e.target.closest(".popup__content");
-  console.log(element);
+  // console.log(element);
   if (!element) {
-    // console.log(popup.classList);
-    popup.classList.remove("visible");
-    popupContent.classList.remove("visible");
-    popupContent.classList.remove("zoom-in");
+    popup.classList.add("hidden");
+    popupContent.classList.remove("scale");
   }
+});
+
+popupClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  popup.classList.add("hidden");
+  popupContent.classList.remove("scale");
 });
